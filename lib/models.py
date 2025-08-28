c7c491d97515bf56c93abc00d7b1878df4f6c299
Danbradleystakusopere <danstakus@gmail.com>
2025-08-28 16:54:08 +0300

    Updated models.py where user can record matches

--- lib/models.py
+++ lib/models.py
@@ -1,5 +1,4 @@
-
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Date
 from sqlalchemy.orm import relationship
 from .db import Base
 
@@ -7,15 +6,20 @@ class Team(Base):
     __tablename__ = "teams"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    players = relationship("Player", back_populates="team")
-    coaches = relationship("Coach", back_populates="team")
+    name = Column(String, unique=True, nullable=False)
+
+    players = relationship("Player", back_populates="team", cascade="all, delete-orphan")
+    coaches = relationship("Coach", back_populates="team", cascade="all, delete-orphan")
+
+    
+    home_matches = relationship("Match", back_populates="home_team", foreign_keys="Match.home_team_id")
+    away_matches = relationship("Match", back_populates="away_team", foreign_keys="Match.away_team_id")
 
 class Player(Base):
     __tablename__ = "players"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    name = Column(String, nullable=False)
     team_id = Column(Integer, ForeignKey("teams.id"))
     team = relationship("Team", back_populates="players")
 
@@ -23,6 +27,29 @@ class Coach(Base):
     __tablename__ = "coaches"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    name = Column(String, nullable=False)
     team_id = Column(Integer, ForeignKey("teams.id"))
     team = relationship("Team", back_populates="coaches")
+
+class Match(Base):
+    __tablename__ = "matches"
+
+    id = Column(Integer, primary_key=True)
+    date = Column(Date, nullable=False)
+    home_team_id = Column(Integer, ForeignKey("teams.id"))
+    away_team_id = Column(Integer, ForeignKey("teams.id"))
+
+    home_team = relationship("Team", foreign_keys=[home_team_id], back_populates="home_matches")
+    away_team = relationship("Team", foreign_keys=[away_team_id], back_populates="away_matches")
+
+    result = relationship("Result", uselist=False, back_populates="match", cascade="all, delete-orphan")
+
+class Result(Base):
+    __tablename__ = "results"
+
+    id = Column(Integer, primary_key=True)
+    match_id = Column(Integer, ForeignKey("matches.id"), unique=True)
+    home_score = Column(Integer, nullable=False)
+    away_score = Column(Integer, nullable=False)
+
+    match = relationship("Match", back_populates="result")
